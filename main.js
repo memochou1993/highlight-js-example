3f9dc72048988f32ca808df4fe63934a8c3bd3af
Memo Chou <memochou1993@gmail.com>
2024-10-05 02:01:30 +0800

    Update demo page

--- main.js
+++ main.js
@@ -1,24 +1,15 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import hljs from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/javascript';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+hljs.registerLanguage('javascript', javascript);
 
-setupCounter(document.querySelector('#counter'))
+const code = `function greet() {
+  console.log("Hello, World!");
+}`;
+
+document.querySelector('#app').innerHTML = `<pre style="text-align: left;">
+<code>${code}</code>
+</pre>`;
+
+hljs.highlightAll();
