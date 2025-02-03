375c02c5be00174815386d9d04b6fa9ac02dc016
Memo Chou <memochou1993@gmail.com>
2025-02-04 01:12:15 +0800

    Add theme switch

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-import './style.css'
 import hljs from 'highlight.js/lib/core';
 import javascript from 'highlight.js/lib/languages/javascript';
+import './style.scss'
+import './highlight.scss'
 
 hljs.registerLanguage('javascript', javascript);
 
@@ -10,6 +11,16 @@ const code = `function greet() {
 
 document.querySelector('#app').innerHTML = `<pre style="text-align: left;">
 <code>${code}</code>
-</pre>`;
+</pre>
+
+<button id="theme-switch">Switch to Light Theme</button>
+`;
 
 hljs.highlightAll();
+
+const themeSwitch = document.querySelector('#theme-switch');
+themeSwitch.addEventListener('click', () => {
+  const html = document.querySelector('html');
+  html.setAttribute('data-theme', html.getAttribute('data-theme') === 'light' ? 'dark' : 'light');
+  themeSwitch.textContent = html.getAttribute('data-theme') === 'light' ? 'Switch to Dark Theme' : 'Switch to Light Theme';
+});
